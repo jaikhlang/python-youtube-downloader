1ec5880dc676dbee509b0149699b1eedc9c6f597
Jaikhlang Brahma <jklangbrahma@users.noreply.github.com>
2016-06-03 02:45:19 +0530

    Adding Index

---
+++ index.py
@@ -0,0 +1,12 @@
+import cgi
+
+print("Content-type: text/html /n/n")
+print("")
+
+print("<html>")
+print("<head><title>Youtube Downloader</title>")
+print("</head>")
+
+print("<body>")
+print("<p>Walla!! It worked!!</p>")
+print("</body>")
